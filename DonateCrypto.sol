a8a177a3a00a356e34fc2259e51776e8d51bd433
GuiiFernandes <guifjj92@gmail.com>
2023-04-12 23:17:11 -0300

    Desafios da aula 1

--- DonateCrypto.sol
+++ DonateCrypto.sol
@@ -1,32 +1,36 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.7;
 
 struct Campaign {
     address author;
     string title;
     string description;
-    string videoUrl;
-    string imageUrl;
+    string[] videoUrl;
+    string[] imageUrl;
     uint256 balance;
+    uint256 goal;
     bool active;
 }
 
 contract DonateCrypto {
   uint256 public nextId = 0;
   mapping(uint256 => Campaign) public campaigns;
+  uint256 public fee = 1;
 
   function addCampaign(
     string calldata title,
     string calldata description,
-    string calldata videoUrl,
-    string calldata imageUrl
+    string[] calldata videoUrl,
+    string[] calldata imageUrl,
+    uint256 goal
   ) public {
     Campaign memory newCampaign;
     newCampaign.title = title;
     newCampaign.description = description;
     newCampaign.videoUrl = videoUrl;
     newCampaign.imageUrl = imageUrl;
+    newCampaign.goal = goal;
     newCampaign.active = true;
     newCampaign.author = msg.sender;
 
@@ -38,6 +42,14 @@ contract DonateCrypto {
     require(msg.value > 0, "You must send a donation value > 0");
     require(campaigns[id].active == true, "Cannot donate to this campaign");
     campaigns[id].balance += msg.value;
+
+    if (campaigns[id].balance >= campaigns[id].goal) {
+      uint256 valueFee = campaigns[id].balance * fee / 100;
+      address payable recipient = payable(campaigns[id].author);
+      recipient.call{value: campaigns[id].balance - valueFee}("");
+      campaigns[id].active = false;
+      campaigns[id].balance = 0;
+    }
   }
 
   function withdraw(uint256 id) public {
@@ -45,13 +57,20 @@ contract DonateCrypto {
     require(campaign.author == msg.sender, "You do not have permission");
     require(campaign.active == true, "This campaign is closed");
     require(
-        campaign.balance > 100,
+        campaign.balance >= 100,
         "This campaign does not have enough balance"
     );
-    uint256 fee = campaign.balance / 10;
+    uint256 valueFee = campaign.balance * fee / 100;
     address payable recipient = payable(campaign.author);
-    recipient.call{value: campaign.balance - fee}("");
+    recipient.call{value: campaign.balance - valueFee}("");
 
     campaigns[id].active = false;
+    campaigns[id].balance = 0;
+  }
+
+  function feeModify(uint256 newFee) public {
+    require(newFee > 0, "The new fee is less than 0");
+    require(newFee <= 100, "The new fee is greater than 100");
+    fee = newFee;
   }
 }
